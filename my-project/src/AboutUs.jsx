40194b9c2a0e4d789fbfbc4f70fbdd8577ae4593
SimNordlund <147151891+SimNordlund@users.noreply.github.com>
2024-12-21 19:42:35 +0100

    changes of responsiv

--- my-project/src/AboutUs.jsx
+++ my-project/src/AboutUs.jsx
@@ -1,7 +1,6 @@
 const people = [
   {
     name: 'Nicke Picke',
-    email: 'leslie.alexander@example.com',
     role: 'Co-Founder / CEO',
     imageUrl:
       'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
@@ -10,7 +9,6 @@ const people = [
   },
   {
     name: 'Simon Sörenssen',
-    email: 'michael.foster@example.com',
     role: 'Co-Founder / CTO',
     imageUrl:
       'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
@@ -19,7 +17,6 @@ const people = [
   },
   {
     name: 'Alice Bejskrew',
-    email: 'dries.vincent@example.com',
     role: 'Business Relations',
     imageUrl:
       'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
@@ -27,7 +24,6 @@ const people = [
   },
   {
     name: 'Karin Kroklund',
-    email: 'lindsay.walton@example.com',
     role: 'Front-end Developer',
     imageUrl:
       'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
@@ -36,7 +32,7 @@ const people = [
   },
   {
     name: 'Tara Trög',
-    email: 'courtney.henry@example.com',
+
     role: 'Designer',
     imageUrl:
       'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
@@ -45,7 +41,6 @@ const people = [
   },
   {
     name: 'Max Hangcock',
-    email: 'tom.cook@example.com',
     role: 'Director of Product',
     imageUrl:
       'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
@@ -55,9 +50,9 @@ const people = [
 
 export default function Example() {
   return (
-    <ul role="list" className="divide-y divide-gray-100">
+    <ul role="list" className="divide-y divide-gray-100 text-center m-2">
       {people.map((person) => (
-        <li key={person.email} className="flex justify-between gap-x-6 py-5">
+        <li key={person.email} className="flex justify-between gap-x-2 py-5">
           <div className="flex min-w-0 gap-x-4">
             <img className="h-12 w-12 flex-none rounded-full bg-gray-50" src={person.imageUrl} alt="" />
             <div className="min-w-0 flex-auto">
