6b7a73579dc6567c6fb155717fc98b55684bd811
SimNordlund <147151891+SimNordlund@users.noreply.github.com>
2025-01-02 11:10:24 +0100

    adjustment of design frontpage

--- my-project/src/Newsletter.jsx
+++ my-project/src/Newsletter.jsx
@@ -1,18 +1,4 @@
-/*
-  This example requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ],
-  }
-  ```
-*/
-import { CalendarDaysIcon, HandRaisedIcon } from '@heroicons/react/24/outline'
+import { PhoneIcon, EnvelopeIcon  } from '@heroicons/react/24/outline'
 
 export default function Newsletter() {
   return (
@@ -43,22 +29,26 @@ export default function Newsletter() {
             </div>
           </div>
           <dl className="grid grid-cols-1 gap-x-8 gap-y-10 sm:grid-cols-2 lg:pt-2">
-            <div className="flex flex-col items-start">
+            <div className="flex flex-col items-center">
               <div className="rounded-md bg-white/5 p-2 ring-1 ring-white/10">
-                <CalendarDaysIcon className="h-6 w-6 text-white" aria-hidden="true" />
+                <PhoneIcon className="h-8 w-8 text-white" aria-hidden="true" />
               </div>
-              <dt className="mt-4 font-semibold text-white">Dagliga vidunder</dt>
-              <dd className="mt-2 leading-7 text-gray-400">
-              Skräll detektor. Call 11-knut
+              <dt className="mt-4 font-semibold text-white">Ring oss</dt>
+              <dd className="mt-2 leading-7 text-gray-300">
+              <a href="tel:07012345678" className="text-gray-300 hover:text-white">
+                070-12345678
+              </a>
               </dd>
             </div>
-            <div className="flex flex-col items-start">
+            <div className="flex flex-col items-center">
               <div className="rounded-md bg-white/5 p-2 ring-1 ring-white/10">
-                <HandRaisedIcon className="h-6 w-6 text-white" aria-hidden="true" />
+                <EnvelopeIcon  className="h-8 w-8 text-white" aria-hidden="true" />
               </div>
-              <dt className="mt-4 font-semibold text-white">Fruntimmer-Stopp</dt>
+              <dt className="mt-4 font-semibold text-white">Mejla oss</dt>
               <dd className="mt-2 leading-7 text-gray-400">
-                Pass på dass.
+              <a href="mailto:travanalys.info@test.se" className="text-gray-300 hover:text-white">
+              travanalys.info@test.se
+              </a>
               </dd>
             </div>
           </dl>
@@ -66,7 +56,7 @@ export default function Newsletter() {
       </div>
       <div className="absolute left-1/2 top-0 -z-10 -translate-x-1/2 blur-3xl xl:-top-6" aria-hidden="true">
         <div
-          className="aspect-[1155/678] w-[72.1875rem] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30"
+          className="aspect-[1155/678] w-[72.1875rem] bg-gradient-to-tr from-[#f1aac8] to-[#776ef3] opacity-30"
           style={{
             clipPath:
               'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
