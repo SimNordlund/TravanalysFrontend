71fa2efddd6f83913e752f0ffd924427a88e1c3d
SimNordlund <147151891+SimNordlund@users.noreply.github.com>
2025-01-07 16:05:52 +0100

    adding FAQ

--- my-project/src/Components/SkrytComponent.jsx
+++ my-project/src/Components/SkrytComponent.jsx
@@ -1,20 +1,21 @@
 import picke2 from '../Bilder/picke2.png';
+import FAQComponent from '../Components/FAQComponent';
+import NewsLetter from '../Newsletter';
 
 export default function Example() {
   return (
-    <section className="isolate overflow-hidden bg-white px-6 py-12 sm:py-16 lg:px-8">
+    <div>
+    <section className="px-6 py-12 sm:py-14 lg:px-8 bg-gray-100">
                   <img
               alt=""
               src={picke2}
               className="mt-0 mx-auto h-41 w-40 rounded-full"
             />
-      <div className="absolute inset-0 -z-10 bg-[radial-gradient(45rem_50rem_at_top,theme(colors.indigo.100),white)] opacity-20" />
-      <div className="absolute inset-y-0 right-1/2 -z-10 mr-16 w-[200%] origin-bottom-left skew-x-[-30deg] bg-white shadow-xl shadow-indigo-600/10 ring-1 ring-indigo-50 sm:mr-28 lg:mr-0 xl:mr-16 xl:origin-center" />
       <div className="mx-auto max-w-2xl lg:max-w-4xl">
-        <figure className="mt-20 ">
+        <figure className="mt-10 ">
           <blockquote className="text-center text-xl font-semibold leading-8 text-gray-900 sm:text-2xl sm:leading-9">
             <p>
-              “Hellre en Arboga i plätten än en Arbogare i stjärten"
+              “Hellre en Arboga i Kläppen än en Arbogare i stjärten"
             </p>
           </blockquote>
           <figcaption className="mt-6">
@@ -29,5 +30,10 @@ export default function Example() {
         </figure>
       </div>
     </section>
+    <div className="mt-2">
+    <FAQComponent/>
+    </div>
+     <NewsLetter/>
+     </div>
   )
 }
