b71fa4dc9c625796d4085607318b52f6acbc30d3
SimNordlund <147151891+SimNordlund@users.noreply.github.com>
2025-01-13 10:57:42 +0100

    removed comments from home and add tables jsx component side

--- my-project/src/Home.jsx
+++ my-project/src/Home.jsx
@@ -1,22 +1,15 @@
-import React, { useState } from 'react';
 import Pricing from './Pricing';
 import Newsletter from './Newsletter';
 import Preview from './Preview';
 import Present from './Present';
 import ToggleComponent from './Components/ToggleComponent';
-import PaginatedLapTable from './Components/PaginatedLapTable';
 
 export default function Home() {
-  const [isModalOpen, setModalOpen] = useState(false);
-
-  // const isLoggedIn = localStorage.getItem('isLoggedIn') === 'true';
-  // const username = localStorage.getItem('username');
 
   return (
     <div>
       <Present />
       <ToggleComponent />
-      <PaginatedLapTable/>
       <Preview />
       <Pricing></Pricing>
       <Newsletter />
---
+++ my-project/src/Tables.jsx
@@ -0,0 +1,12 @@
+import PaginatedLapTable from "./Components/PaginatedLapTable";
+import Preview from "./Preview";
+
+export default function Tables() {
+
+return (
+  <div>
+    <Preview/>
+    <PaginatedLapTable/>
+  </div>
+);
+} 
