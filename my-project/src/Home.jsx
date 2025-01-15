ef6ba181f51223eaf2a89fa9617bddf9de838fc4
SimNordlund <147151891+SimNordlund@users.noreply.github.com>
2025-01-15 11:47:26 +0100

    clean up

--- my-project/src/Home.jsx
+++ my-project/src/Home.jsx
@@ -1,10 +1,6 @@
 import Pricing from './Pricing';
-import Newsletter from './Newsletter';
-import Preview from './Preview';
 import Present from './Present';
-import ToggleComponent from './Components/ToggleComponent';
 import PreviewFrontPage from './PreviewHomePage';
-import ChartPage from './ChartPage';
 
 export default function Home() {
 
