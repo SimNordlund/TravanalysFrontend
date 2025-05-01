165847b2bb4a77fc0bb904b8e16006a32f7d37cf
SimNordlund <147151891+SimNordlund@users.noreply.github.com>
2025-05-01 16:10:20 +0200

    adding button

--- my-project/src/Components/ToggleComponent.jsx
+++ my-project/src/Components/ToggleComponent.jsx
@@ -22,12 +22,15 @@ const ToggleComponent = () => {
     { id: 1, name: "Diagram", bgColor: "bg-gradient-to-r from-blue-400 via-blue-500 to-blue-600", view: "bar" },
     { id: 2, name: "Tabell",  bgColor: "bg-gradient-to-r from-purple-400 via-purple-500 to-purple-600", view: "table" },
     { id: 3, name: "Analys",  bgColor: "bg-gradient-to-r from-indigo-400 via-indigo-500 to-indigo-600", view: "spider" },
+    { id: 3, name: "Skrällar",  bgColor: "bg-gradient-to-r from-orange-400 via-orange-500 to-orange-600", view: "spider" },
   ];
 
   return (
     <div className="text-center pt-12 pb-12 sm:pt-16 sm:pb-14">
       {/* Buttons */}
-      <div className="flex justify-center gap-x-5 sm:gap-x-14 flex-wrap mb-5 sm:mb-8">
+
+      <div className="flex justify-center gap-x-3.5 sm:gap-x-10 flex-nowrap overflow-auto mb-4 sm:mb-8 pt-3 pb-3">
+
         {callouts.map((c) => (
           <div
             key={c.id}
@@ -37,7 +40,7 @@ const ToggleComponent = () => {
             <div
               className={`
                 ${c.bgColor}
-                relative h-14 w-24 sm:w-72 sm:h-18
+                relative h-14 w-20 sm:w-72 sm:h-18 mb-1 sm:mb-0 
                 overflow-hidden rounded-md flex items-center justify-center
                 transition-all duration-300 shadow-xl
                 ${selectedView === c.view
