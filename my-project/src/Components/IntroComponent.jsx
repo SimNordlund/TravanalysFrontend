d65a18ac7f6c73799b5652fa7f2c88919eac32db
SimNordlund <147151891+SimNordlund@users.noreply.github.com>
2025-05-09 14:21:38 +0200

    intro picture adjustment

--- my-project/src/Components/IntroComponent.jsx
+++ my-project/src/Components/IntroComponent.jsx
@@ -27,10 +27,10 @@ const features = [
 
 export default function IntroComponent() {
   return (
-    <div className="overflow-hidden bg-white pt-6 pb-10 sm:pt-16 sm:pb-20">
+    <div className="overflow-hidden bg-white pt-6 pb-10 sm:pt-20 sm:pb-20">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
-          <div className="lg:pt-4 lg:pr-8">
+          <div className="lg:pt-0 lg:pr-8">
             <div className="lg:max-w-lg">
               <h2 className="text-base/7 font-semibold text-indigo-600">Överlägsen analys</h2>
               <p className="mt-2 text-4xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-5xl">
