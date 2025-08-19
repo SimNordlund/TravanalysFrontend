adca89892db6d1ad819f85fa4f25092cbceb40d6
SimNordlund <147151891+SimNordlund@users.noreply.github.com>
2025-08-19 15:42:38 +0200

    frontpage update moving picture

--- my-project/src/Components/IntroComponent.jsx
+++ my-project/src/Components/IntroComponent.jsx
@@ -79,7 +79,7 @@ export default function IntroComponent() {
             src={vinst1}
             width={2432}
             height={1442}
-            className="hidden sm:block w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-24 lg:-ml-24"
+            className="hidden sm:block w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-24 lg:-ml-24 mt-9"
           />
         </div>
       </div>
