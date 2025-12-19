9467e953025efe6f7bd4fa9f6be90f71bb5e0001
SimNordlund <147151891+SimNordlund@users.noreply.github.com>
2025-12-19 17:34:26 +0100

    label update

--- my-project/src/Newsletter.jsx
+++ my-project/src/Newsletter.jsx
@@ -199,7 +199,7 @@ export default function Newsletter() {
                   href="https://www.atg.se/kopandel/spel/325442_V85_2025-12-20_18_3"
                   className="text-gray-300 hover:text-white"
                 >
-                  travanalys.se - V86
+                  travanalys.se - V85
                 </a>
               </dd>
             </div>
